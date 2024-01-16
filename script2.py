f752d12ab0ef8e4a1f979cf119c11236521e9b39
Ish1126 <ishika.thakur@flemingcollege.ca>
2024-01-16 09:26:52 -0500

    Put comment back in script2.py
    Comment was unintentionally deleted by revert.

--- script2.py
+++ script2.py
@@ -1,2 +1,2 @@
-print("There should be one-- and preferably only one --obvious way to do it.")
-print("Although that way may not be obvious at first unless you're Dutch.")
+  # This is good programming advice
+  print("If the implementation is hard to explain, it's a bad idea.")
