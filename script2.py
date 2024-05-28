5175f3ce81115242b1f2795ed5ef1b5d6acc63e8
Ish1126 <156629367+Ish1126@users.noreply.github.com>
2024-05-28 13:06:53 -0400

    added comment to script2.py

--- script2.py
+++ script2.py
@@ -1,3 +1,5 @@
 # This is good programming advice
 print("If the implementation is hard to explain, it's a bad idea.")
 print("If the implementation is hard to explain, it may be a good idea.")
+print("Sparse is better than dense.")
+print("Flat is better than nested.")
