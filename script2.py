9ac4db9b8e17c92f4c896ede5e7a492bcf11441e
Ish1126 <156629367+Ish1126@users.noreply.github.com>
2024-05-29 00:28:30 -0400

    Modified both files in the repo clone
    Deleted a print() statement from script1.py
    Added two print() statements to script2.py

--- script2.py
+++ script2.py
@@ -2,3 +2,4 @@
 print("If the implementation is hard to explain, it's a bad idea.")
 print("If the implementation is hard to explain, it may be a good idea.")
 print("Sparse is better than dense.")
+print("Flat is better than nested.")
