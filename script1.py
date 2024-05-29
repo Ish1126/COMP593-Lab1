c4b5a5645d87c24e879a8b899e4ff43313354d17
Ish1126 <156629367+Ish1126@users.noreply.github.com>
2024-05-29 00:27:07 -0400

    dd

--- script1.py
+++ script1.py
@@ -1,2 +1,3 @@
 #Print lines from Zen of Python.
 print("beautiful is better than ugly.")
+print("simple is better than complex.")
--- script2.py
+++ script2.py
@@ -2,4 +2,3 @@
 print("If the implementation is hard to explain, it's a bad idea.")
 print("If the implementation is hard to explain, it may be a good idea.")
 print("Sparse is better than dense.")
-print("Flat is better than nested.")
